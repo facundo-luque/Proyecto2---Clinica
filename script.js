d8371ba9941899b4e9a0eb89b772b17cfd62d632
antop <antomessinaa@gmail.com>
2023-12-18 20:03:23 -0300

    pag principal

---
+++ script.js
@@ -0,0 +1,21 @@
+let listElements = document.querySelectorAll('.list-click');
+listElements.forEach(listElement => {
+    listElement.addEventListener( 'click', () => {
+
+       listElement.classList.toggle('arrow');
+
+       let height = 0;
+       let menu = listElement.nextElementSibling;
+       
+       if (menu.clientHeight == "0") {
+        height=menu.scrollHeight;
+       }
+
+       menu.style.height = `${height}px`;
+   });
+});
+
+document.getElementById('lupa').addEventListener('click', function() {
+    document.getElementById('search').select();
+    
+});
